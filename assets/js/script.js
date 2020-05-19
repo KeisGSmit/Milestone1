201040d78c3b539a87ef20f3f458cfd2a2df8677
KeisGSmit <kgsmit98@gmail.com>
2020-05-19 13:24:04 +0000

    Redesign 3 - added 'hero image' logo, need to add more images and probably a executive message

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,5 @@
-function validateForm(){
-  var a = document.forms["myform"]["first_name"].value;
-  var b = document.forms["myform"]["last_name"].value;
-  var c = document.forms["myform"]["email_address"].value;
-  var d = document.forms["myform"]["donation"].value;
-  if (a == "" || a == null) {
-    alert("First name must be filled out");
-    return false;
-  }  
-  if (b == "" || b == null) {
-    alert("Last name must be filled out");
-    return false;
-  }
-  if (c == "" || c == null) {
-    alert("Email address must be provided");
-    return false;
-  }
-  if (d == "" || d == null) {
-    alert("Why are you not donating money?");
-    return false;
-  }
-  }
+  $('#contact-form').submit(function(e) {
+            e.preventDefault();
+            $('#staticBackdrop').modal('show');
+            $('#contact-form')[0].reset();
+        });
